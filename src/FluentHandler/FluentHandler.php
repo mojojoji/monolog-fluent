59cc8d88926ae1d3e29f593b84ecaa8602026a1e
joji <jojimail@gmail.com>
2016-04-13 19:19:24 +0530

    Remove message from tag

--- src/FluentHandler/FluentHandler.php
+++ src/FluentHandler/FluentHandler.php
@@ -50,7 +50,7 @@ class FluentHandler extends AbstractProcessingHandler
     public function write(array $record)
     {
         $record['level'] = Logger::getLevelName($record['level']);
-        $tag  = 'monolog.' . $record['channel'] . '.' . $record['message'];
+        $tag  = 'monolog.' . $record['channel'];
         $this->logger->post($tag, $record);
     }
 }
